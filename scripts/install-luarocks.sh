82670ba8964e69a3c12e130d46a56c3a5e665457
David P. Sicilia <dpsicilia@gmail.com>
2026-08-16 16:01:53 -0400

    changes

--- scripts/install-luarocks.sh
+++ scripts/install-luarocks.sh
@@ -7,10 +7,11 @@ set -e
 lua_version=5.4
 
 rocks='
-    ftcsv
-    luaposix
-    lunajson
-    redis-lua
+  argparse
+  ftcsv
+  luaposix
+  lunajson
+  redis-lua
 '
 
 luarocks config lua_version "$lua_version"
@@ -21,18 +22,18 @@ echo "$lua_v_cmd"
 $lua_v_cmd
 
 for rock in $rocks; do
-    echo "installing luarock: $rock"
-    luarocks install "$rock" --local
+  echo "installing luarock: $rock"
+  luarocks install "$rock" --local
 done
 
 if [[ ! -d ~/.luarocks/lib/luarocks/rocks-$lua_version/lua-cityhash ]]; then
-    echo "installing lua-cityhash..."
-    pushd /tmp
-    rm -rf lua-cityhash
-    git clone https://github.com/csfrancis/lua-cityhash.git
-    cd lua-cityhash
-    luarocks make --local lua-cityhash-1.0-1.rockspec
-    popd
+  echo "installing lua-cityhash..."
+  pushd /tmp
+  rm -rf lua-cityhash
+  git clone https://github.com/csfrancis/lua-cityhash.git
+  cd lua-cityhash
+  luarocks make --local lua-cityhash-1.0-1.rockspec
+  popd
 else
-    echo "lua-cityhash already installed."
+  echo "lua-cityhash already installed."
 fi
