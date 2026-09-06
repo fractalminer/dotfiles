de01c7000c0a35701f22e18f44d0ca6fa5c5354d
David P. Sicilia <dpsicilia@gmail.com>
2026-09-06 12:07:27 -0400

    changes

--- dot/config/lua/startup.lua
+++ dot/config/lua/startup.lua
@@ -24,7 +24,13 @@ local function path_add( fmt, ... )
   package.path = format( '%s;%s', package.path, what )
 end
 
-path_add( '%s%s', HOME, '/dev/moonlib/?.lua' )
+local function cpath_add( fmt, ... )
+  local what = format( fmt, ... )
+  package.cpath = format( '%s;%s', package.cpath, what )
+end
+
+path_add(  '%s%s', HOME, '/dev/moonlib/?.lua' )
+cpath_add( '%s%s', HOME, '/.local/lib/lua/5.4/?.so' )
 
 -----------------------------------------------------------------
 -- Globals.
