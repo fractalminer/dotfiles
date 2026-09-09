8a0f6d025d9df9703233d3ed8aba25632059a494
David P. Sicilia <dpsicilia@gmail.com>
2026-09-08 22:22:05 -0400

    Install nerd fonts.

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -eo pipefail
+set -x
 # Install fonts in user's home folder and update font cache.
 #
 # It is assumed that this script will be run with CWD one level
@@ -6,23 +8,40 @@
 
 source scripts/utils.sh
 
-if [[ -e ~/.local/share/fonts/"Literation Mono Powerline.ttf" ]]; then
-  msg "Powerline fonts already installed."
-  exit 0
+# NOTE: this one is deprecated, we're now using the NERD version
+# below since it has a lot of extra icons.
+if [[ ! -e ~/.local/share/fonts/"Literation Mono Powerline.ttf" ]]; then
+  msg "Powerline fonts not installed... installing"
+
+  cd /tmp
+  rm -rf fonts
+
+  git clone https://github.com/powerline/fonts
+  cd fonts
+
+  # This should update the font cache, which should then also pick
+  # up the fonts in the ~/.fonts folder that will now exist after
+  # the symlinks are set up into the .dotfiles folder.
+  ./install.sh
+
+  # Reset the font cache do this:
+  fc-cache -fv; check "update font cache"
 fi
 
-msg "Installing powerline fonts."
+# ---------------------------------------------------------------
+# Install Nerd Font.
+# ---------------------------------------------------------------
+if [[ ! -e ~/.local/share/fonts/LiterationMono ]]; then
+  msg "NERD Liberation Mono fonts not installed... installing"
 
-cd /tmp
-rm -rf fonts
+  cd /tmp
+  rm -f LiberationMono.tar.xz
 
-git clone https://github.com/powerline/fonts
-cd fonts
+  wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/LiberationMono.tar.xz"
 
-# This should update the font cache, which should then also pick
-# up the fonts in the ~/.fonts folder that will now exist after
-# the symlinks are set up into the .dotfiles folder.
-./install.sh
+  mkdir -p ~/.local/share/fonts/LiterationMono
+  tar xf LiberationMono.tar.xz -C ~/.local/share/fonts/LiterationMono
 
-# Note: to reset the font cache do this:
-#fc-cache -fv; check "update font cache"
+  # Reset the font cache do this:
+  fc-cache -fv; check "update font cache"
+fi
